PixelExperience/frameworks_base

PixelPropsUtils: Spoof Pixel 7 Pro to some Google apps
This unlocks some non-TPU reliant, useful features across all devices.

@jhenrique09: Update some apps

Change-Id: Ieb09625d5d0d2d5f7036db4ae5a0036b9e4a48fa

core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -43,13 +43,22 @@ public class PixelPropsUtils {
     private static final Map<String, ArrayList<String>> propsToKeep;
 
     private static final String[] packagesToChangePixel7Pro = {
+            "com.google.android.inputmethod.latin",
+            "com.google.android.setupwizard",
+            "com.google.android.apps.turbo",
+            "com.google.android.googlequicksearchbox",
             "com.google.android.apps.wallpaper",
-            "com.google.android.apps.privacy.wildlife"
+            "com.google.android.apps.wallpaper.pixel",
+            "com.google.android.apps.privacy.wildlife",
+            "com.google.android.apps.googleassistant",
+            "com.google.android.apps.nbu.files",
+            "com.google.android.apps.podcasts",
+            "com.google.android.contacts",
+            "com.google.android.deskclock"
     };
 
     private static final String[] packagesToChangePixelXL = {
-            "com.google.android.apps.photos",
-            "com.google.android.inputmethod.latin"
+            "com.google.android.apps.photos"
     };
 
     private static final String[] extraPackagesToChange = {
@@ -173,6 +182,8 @@ public class PixelPropsUtils {
                     setPropValue("DEVICE", "marlin");
                     setPropValue("MODEL", "Pixel XL");
                     setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
+                }else{
+                    propsToChangePixel7Pro.forEach((k, v) -> setPropValue(k, v));
                 }
                 return;
             }

pnplusplus/android_frameworks_base-awaken

core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -63,15 +63,24 @@ public class PixelPropsUtils {
     private static final Map<String, ArrayList<String>> propsToKeep;
 
     private static final String[] packagesToChangePixel8Pro = {
+            "com.google.android.apps.aiwallpapers",
             "com.google.android.apps.customization.pixel",
-            "com.google.android.apps.privacy.wildlife",
-            "com.google.android.apps.wallpaper.pixel",
-            "com.google.android.apps.wallpaper",
-            "com.google.android.apps.subscriptions.red",
-            "com.google.pixel.livewallpaper",
-            "com.google.android.wallpaper.effects",
             "com.google.android.apps.emojiwallpaper",
-            "com.google.android.apps.aiwallpapers"
+            "com.google.android.apps.googleassistant",
+            "com.google.android.apps.nbu.files",
+            "com.google.android.apps.podcasts",
+            "com.google.android.apps.privacy.wildlife",
+            "com.google.android.apps.subscriptions.red",
+            "com.google.android.apps.turbo",
+            "com.google.android.apps.wallpaper",
+            "com.google.android.apps.wallpaper.pixel",
+            "com.google.android.contacts",
+            "com.google.android.deskclock",
+            "com.google.android.googlequicksearchbox",
+            "com.google.android.inputmethod.latin",
+            "com.google.android.setupwizard",
+            "com.google.android.wallpaper.effects",
+            "com.google.pixel.livewallpaper"
     };
 
     private static final String[] extraPackagesToChange = {